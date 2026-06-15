19bf38bb1585f1d3833f14384a410d183c372a49
adamooadamoo650-ctrl <adamooadamoo650@gmail.com>
2026-06-15 14:28:50 +0100

    Update config.js
    HELLO

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
+
 module.exports = {
     bot: {
         tokens: [
           "YOUR_BOT_TOKEN_HERE",              // Main bot token - Replace with your actual token
-        //  "YOUR_SECOND_BOT_TOKEN_HERE",      // 2 bot token - Uncomment and replace to use
+        //  "YOUR_SECOمعلش,      // 2 bot token - Uncomment and replace to use
         //  "YOUR_THIRD_BOT_TOKEN_HERE",       // 3 bot token - Uncomment and replace to use
         //  "YOUR_FOURTH_BOT_TOKEN_HERE",      // 4 bot token - Uncomment and replace to use
         //  "YOUR_FIFTH_BOT_TOKEN_HERE",       // 5 bot token - Uncomment and replace to use
